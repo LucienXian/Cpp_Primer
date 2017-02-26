9738926ac21b2728b1292017a2fa31ad641d8974
Lucien <LucienXian97@gmail.com>
2017-02-26 22:30:39 +0800

    chapter 1(1_23)

---
+++ Cpp_Primer/C01/1_23.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include <iostream>
+#include "Sale_item.h"
+
+using namespace std;
+
+int main()
+{
+	Sales_item item, New_item;
+	if(cin >> item)
+	{
+		int count = 1;
+		while (cin >> New_item)
+		{
+			if (item.isbn == New_item.isbn)
+				count++;
+			else
+			{
+				cout << item.isbn << " count: " << count << endl;
+				count = 1;
+				item = New_item;
+			}
+		}
+		return 0;
+	}
+	else
+	{
+		cerr << "No Data";
+	}
+}
